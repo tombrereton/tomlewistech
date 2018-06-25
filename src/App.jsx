b13c12e744ccf4daac0949301373c95ebb092836
tombrereton <tbrereton89@gmail.com>
2018-06-25 12:56:16 +0100

    Add navigation component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Navigation/Navigation.jsx';
+
+const App = () => (
+  <div className='app'>
+    <h1>React Router Demo</h1>
+    <Navigation />
+  </div>
+);
+
+export default App;

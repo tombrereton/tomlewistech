94624eb0712b78c4627ffb15c9b47b255d18cf97
Tom Brereton <Tom.Brereton@asos.com>
2018-06-25 20:01:20 +0100

    Update nav to burger menu

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,34 +1,71 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { css } from 'emotion'
+import { elastic as Menu } from 'react-burger-menu'
 
-const Navigation = () => (
-  <nav>
-    <ul className={navBarList}>
-      <li><NavLink exact activeClassName={current} to='/'>Home</NavLink></li>
-      <li><NavLink exact activeClassName={current} to='/projects'>Projects</NavLink></li>
-      <li><NavLink exact activeClassName={current} to='/blog'>Blog</NavLink></li>
-      <li><NavLink exact activeClassName={current} to='/about'>About</NavLink></li>
-      <li><NavLink exact activeClassName={current} to='/contact'>Contact</NavLink></li>
-    </ul>
-  </nav>
-);
+class Navigation extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Menu right styles={styles} className={navBarList}>
+        <nav>
+          <ul>
+            <li><NavLink exact to='/'>Home</NavLink></li>
+            <li><NavLink exact to='/projects'>Projects</NavLink></li>
+            <li><NavLink exact to='/blog'>Blog</NavLink></li>
+            <li><NavLink exact to='/about'>About</NavLink></li>
+            <li><NavLink exact to='/contact'>Contact</NavLink></li>
+          </ul>
+        </nav>
+      </Menu>
+    )
+  }
+};
+
+var styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    right: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmItem: {
+    display: 'inline-block'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
 
 const navBarList = css({
-  position: 'fixed',
-  top: 0,
-  width: '100%',
-  listStyleType: 'none',
-  margin: 0,
-  padding: 0,
-  overflow: 'hidden',
-  backgroundColor: '#333',
-  '& li': {
-    float: 'left',
-    borderRight: '1px solid #bbb'
-  },
-  '& li:last-child': {
-    borderRight: 'none'
+  '& ul': {
+    listStyleType: 'none'
   },
   '& li a': {
     display: 'block',
@@ -37,9 +74,6 @@ const navBarList = css({
     padding: '14px 16px',
     textDecoration: 'none'
   },
-  '& li a:hover': {
-    backgroundColor: '#111'
-  }
 })
 
 const current = css({

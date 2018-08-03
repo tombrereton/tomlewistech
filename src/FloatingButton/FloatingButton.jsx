421fe11642093de468b2440e91bd64e4021885e3
tombrereton <tbrereton89@gmail.com>
2018-08-03 20:50:51 +0100

    Modify button to add space in front of text

--- src/FloatingButton/FloatingButton.jsx
+++ src/FloatingButton/FloatingButton.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
-import { Slide } from '@material-ui/core';
+import { Slide } from "@material-ui/core";
+import {BorderLeft} from '@material-ui/icons'
 
-
-const styles = theme => ({
+const styles = {
   button: {
-    position: 'fixed',
-    bottom: 10,
+    borderRadius: 5,
+    position: "fixed",
+    bottom: 10
   },
-});
+  buttonText: {
+    paddingLeft: 10
+  }
+}
 
 function ContactButton(props) {
   const { classes } = props;
   return (
-    <Slide direction="up" in style={{transitionDelay:301}}>
-      <Button variant="extendedFab" aria-label="delete" className={classes.button} color='secondary'>
+    <Slide direction="up" in style={{ transitionDelay: 301 }}>
+      <Button
+        variant="extendedFab"
+        aria-label="delete"
+        className={classes.button}
+        color="secondary"
+      >
         <QuestionAnswerIcon />
-        {props.text}
+        <span className={classes.buttonText}>{props.text}</span>
       </Button>
     </Slide>
   );
 }
 
 ContactButton.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(ContactButton);

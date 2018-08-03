36caddfacdc392b7fff4aaa52609a2e3adf4c910
tombrereton <tbrereton89@gmail.com>
2018-08-03 21:12:01 +0100

    Add email to contact page

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import HighlightText from '../HighlightText/HighlightText';
+import React from "react";
+import HighlightText from "../HighlightText/HighlightText";
 import { css } from "react-emotion";
 
 const contactContainer = css({
-    display: 'flex',
-    flexDirection: 'column',
-    flex: 1,
-    alignItems: 'flex-start',
-    zIndex: -1,
-    backgroundColor: "#EEEEEF",
-    color: "#727373",
-    padding: "1em"
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  alignItems: "flex-start",
+  zIndex: -1,
+  backgroundColor: "#EEEEEF",
+  color: "#727373",
+  padding: "1em"
 });
 
 const Contact = () => (
-    <div className={contactContainer}>
-        <HighlightText color="#C5EBF1" text="Contact Me" type="h1" />
-        <p>You can reach me via email: <strong>hello@example.com</strong></p>
-    </div>
+  <div className={contactContainer}>
+    <HighlightText color="#C5EBF1" text="Contact Me" type="h1" />
+    <p>
+      You can reach me via email: 
+    </p>
+    <a href="mailto:tom@tomlewis.tech?Subject=Website%20Project" target="_top">
+      tom@tomlewis.tech
+    </a>
+  </div>
 );
 
 export default Contact;

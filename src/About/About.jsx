319cfa427d414bc021369839282e2cfe5860126d
tombrereton <tbrereton89@gmail.com>
2018-08-09 12:51:06 +0100

    Update about blurb

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -47,7 +47,7 @@ class About extends React.Component {
 
   render() {
     const { classes } = this.props;
-    let skills = this.state.skills.map((skill) => {
+    let skills = this.state.skills.map(skill => {
       return (
         <Grid item xs={12} sm={6} md={4} lg={3} className={classes.gridItem} key={skill.id}>
           <AboutCard
@@ -71,15 +71,22 @@ class About extends React.Component {
         <div className={classes.title}>
           <HighlightText color="#C5EBF1" text="About Me" type="h1" />
         </div>
-        <img className={classes.portraitContainer} src={portraitPhoto} alt={"A portrait of Tom Lewis"} />
+        <img
+          className={classes.portraitContainer}
+          src={portraitPhoto}
+          alt={"A portrait of Tom Lewis"}
+        />
         <Grid item xs={12} sm={6} md={4} lg={3} className={classes.gridItem}>
-          <Typography style={{paddingBottom: "1em"}} variant="subheading">
+          <Typography style={{ paddingBottom: "1em" }} variant="subheading">
             I'm a software engineer who loves design and building things. I primarily use .NET Core,
-            React, and Python to develop and Microsoft Azure for cloud services.          </Typography>
+            React, and Python to develop and Microsoft Azure for cloud services.{" "}
+          </Typography>
           <Typography variant="subheading">
-            I currently work at ASOS in a back-end team using the .NET Framework and Microsoft
-            Azure. Outside of work, I enjoy building front-end web apps with React and CSS-in-JS such as this
-            website.
+            I currently work at ASOS in one of the back-end teams using .NET Framework and Microsoft
+            Azure. We optimise our back-end to handle high traffic whilst still minimising costs
+            through efficient architecture design and technologies such as; AKS, Docker, .NET Core,
+            Team City, and Octopus. Outside of work, I enjoy building front-end web apps with React,
+            CSS-in-JS, and Google Firebase such as this website.
           </Typography>
         </Grid>
         {skills}

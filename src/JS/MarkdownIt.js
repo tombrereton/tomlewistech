de8bbce45c5b351b29d75c31a6aa19d197733516
tombrereton <tbrereton89@gmail.com>
2018-08-11 23:51:54 +0100

    Enabled centered images using implicit figures in MD

--- src/JS/MarkdownIt.js
+++ src/JS/MarkdownIt.js
@@ -1,6 +1,7 @@
 import markdownIt from "markdown-it";
 import imSize from "markdown-it-imsize";
 import markdownItTocAndAnchor from "markdown-it-toc-and-anchor-with-slugid";
+import implicitFigures from "markdown-it-implicit-figures"
 import uslug from "uslug";
 
 var markdown = markdownIt({
@@ -14,6 +15,12 @@ markdown.use(markdownItTocAndAnchor, {
   slugify: header => {
     return encodeURIComponent(uslug(header));
   }
+});
+markdown.use(implicitFigures, {
+  dataType: false,  // <figure data-type="image">, default: false
+  figcaption: true,  // <figcaption>alternative text</figcaption>, default: false
+  tabindex: false, // <figure tabindex="1+n">..., default: false
+  link: false // <a href="img.png"><img src="img.png"></a>, default: false
 });
 
 export default markdown;

2d90a0ff8a0995f73220d4a6f91ba6179aa8b953
tombrereton <tbrereton89@gmail.com>
2018-09-18 18:00:32 +0100

    Change Home to use Material css

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,47 +1,46 @@
 import React from "react";
-import { css } from "react-emotion";
 import ContactButton from "../ContactButon/ContactButton";
 import HighlightText from "../HighlightText/HighlightText";
+import HomeCard from "../HomeCard/HomeCard";
+import { withStyles } from "@material-ui/core/styles";
 
-const homeContainer = css({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  flex: 1,
-  padding: "1em",
-  alignItems: "center",
-  color: "#727373",
-  textAlign: "center"
-});
-
-const section1 = css({});
-
-const section2 = css({
-  marginTop: "3em"
-});
-
-const section3 = css({
-  marginTop: "1em"
-});
-
-class Home extends React.Component {
-
-  render() {
-    return (
-      <div className={homeContainer}>
-        <div className={section1}>
-          <HighlightText color="#C5EBF1" text="Hi I'm Tom." type="h1" />
-        </div>
-        <div className={section2}>
-          <HighlightText color="#C5EBF1" text="A web developer &" type="h1" />
-        </div>
-        <div className={section3}>
-          <HighlightText color="#C5EBF1" text="software engineer." type="h1" />
-        </div>
-        <ContactButton text="Contact Me" />
-      </div>
-    );
-  }
+const styles = {
+  homeContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    flex: 1,
+    padding: "1em",
+    alignItems: "center",
+    color: "#727373",
+    textAlign: "center"
+  },
+  section1: {
+  },
+  section2: {
+    marginTop: "3em"
+  },
+  section3: {
+    marginTop: "1em"
+  },
 }
 
-export default Home;
+function Home(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.homeContainer}>
+      <div className={classes.section1}>
+        <HighlightText color="#C5EBF1" text="Hi I'm Tom." type="h1" />
+      </div>
+      <div className={classes.section2}>
+        <HighlightText color="#C5EBF1" text="A web developer &" type="h1" />
+      </div>
+      <div className={classes.section3}>
+        <HighlightText color="#C5EBF1" text="software engineer." type="h1" />
+      </div>
+      <ContactButton text="Contact Me" />
+    </div>
+  );
+}
+
+export default withStyles(styles)(Home);
--- src/HomeCard/HomeCard.jsx
+++ src/HomeCard/HomeCard.jsx
@@ -1,29 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 
 const styles = {
-  skillContainer: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    paddingTop: "2em",
-    paddingBottom: "2em"
+  homeCardContainer: {
+    background: "black",
+    width: "100vw",
   }
-};
+}
 
 function HomeCard(props) {
-  const classes = props;
+  const { classes } = props;
   return (
     <div className={classes.homeCardContainer}>
-      <div className={classes.cropContainer}>
-        <img src={props.children} alt={"Image of" + props.pageTitle} />
-      </div>
-      <Typography variant="title" style={{ paddingTop: "0.5em", paddingBottom: "0.3em" }}>
-        {props.pageTitle}
-      </Typography>
-      <Typography variant="subheading">{props.pageSummary}</Typography>
+      Projects
     </div>
   );
 }

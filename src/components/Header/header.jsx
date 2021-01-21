7dccc4e5c9b1b7cab931e9a45b92c344b51c9ef2
tombrereton <tbrereton89@gmail.com>
2021-01-21 18:10:54 +0000

    Fix header and add service placeholders

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,10 +3,14 @@ import logo from "./logo.png";
 import styles from "./header.module.css";
 
 export default () => (
-  <div className={styles.header}>
-    <div className={styles.headerLeft}>
-      <img className={styles.headerLogo} src={logo} />
-      <p className={styles.company}>Tom Lewis Tech</p>
+    <div>
+        <div className={styles.header}>
+            <div className={styles.headerLeft}>
+                <img className={styles.headerLogo} src={logo} />
+                <p className={styles.company}>Tom Lewis Tech</p>
+            </div>
+        </div>
+        <div className={styles.emptyDiv}>
+        </div>
     </div>
-  </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,12 @@ export default () => (
     <h1 className={styles.heading}>Hello, I'm <span className={styles.highlight}>Tom</span></h1>
     <h3 className={styles.subHeading}>Web Developer & Online Presence Consultant</h3>
     <h2 className={styles.sectionHeading}>How I Can <span className={styles.highlight}>Help</span></h2>
-    <Service image={responsive} text={"Build you a fast, responsive website. Optimised for both mobile and desktop."}/>
+    <div>
+      <Service image={responsive} text={"Build you a fast, responsive and unique website. Optimised for both mobile and desktop."} />
+      <Service image={responsive} text={"Setup Google or Instagram ads to increase your traffic massively."} />
+      <Service image={responsive} text={"Put you on Google Maps with a full business profile to maximise your presence "} />
+      <Service image={responsive} text={"Add analytics to track website traffic and measure online impact"} />
+      <Service image={responsive} text={"Help setup a business Newsletter to retain existing customers"} />
+    </div>
   </div>
 )

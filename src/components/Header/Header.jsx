3aab7b12cbace00595b49bf9689a2e1513546ae2
tombrereton <tbrereton89@gmail.com>
2021-01-21 00:55:30 +0000

    Fix header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.png';
+import styles from './header.module.css';
+
+export default () => (
+    <div className={styles.header}>
+        <div className={styles.headerLeft}>
+            <img src={logo} />
+            <p className={styles.company}>Tom Lewis Tech</p>
+        </div >
+    </div >
+)

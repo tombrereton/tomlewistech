52cc1535a663db1f19935c113bbdf90c9d79090b
tombrereton <tbrereton89@gmail.com>
2021-01-22 17:35:35 +0000

    Update service descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ export default () => (
     <h1 className={styles.heading}>Hello, I'm <span className={`${styles.highlight} ${styles.underline}`}>Tom.</span></h1>
     <h3 className={styles.subHeading}>Web Developer & Online Presence Consultant.</h3>
     <h2 className={styles.sectionHeading}>How I Can <span className={styles.highlight}>Help</span></h2>
-    <Service image={responsive} text={"Build you a fast, responsive and unique website. Optimised for both mobile and desktop."} />
+    <Service image={responsive} text={"Build you a fast and intuitive website. Optimised for both mobile and desktop."} />
     <Service image={responsive} text={"Setup Google or Instagram ads to increase your traffic massively."} />
-    <Service image={responsive} text={"Put you on Google Maps with a full business profile to maximise your presence "} />
-    <Service image={responsive} text={"Add analytics to track website traffic and measure online impact"} />
-    <Service image={responsive} text={"Help setup a business Newsletter to retain existing customers"} />
+    <Service image={responsive} text={"Put you on Google Maps with a full business profile to maximise your presence."} />
+    <Service image={responsive} text={"Add analytics to track website traffic and measure online impact."} />
+    <Service image={responsive} text={"Design & Build complex software solutions where bespoke functionality is needed."} />
     <h2 className={styles.sectionHeading}>Some <span className={styles.highlight}>Projects</span></h2>
     <Project image={soundsort} url="https://soundsort.net"/>
     <BigLink />

b4a5d582cf6d107694dd7bb4bb1807de3403e118
tombrereton <tbrereton89@gmail.com>
2021-01-22 16:00:27 +0000

    Add underline animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import BigLink from '../components/BigLink/bigLink'
 
 export default () => (
   <div className={styles.home}>
-    <h1 className={styles.heading}>Hello, I'm <span className={`${styles.highlight} ${styles.underline}`}>Tom</span></h1>
-    <h3 className={styles.subHeading}>Web Developer & Online Presence Consultant</h3>
+    <h1 className={styles.heading}>Hello, I'm <span className={`${styles.highlight} ${styles.underline}`}>Tom.</span></h1>
+    <h3 className={styles.subHeading}>Web Developer & Online Presence Consultant.</h3>
     <h2 className={styles.sectionHeading}>How I Can <span className={styles.highlight}>Help</span></h2>
     <Service image={responsive} text={"Build you a fast, responsive and unique website. Optimised for both mobile and desktop."} />
     <Service image={responsive} text={"Setup Google or Instagram ads to increase your traffic massively."} />

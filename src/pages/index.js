fd469885b583d0ab63a4d0d77124d5e79553b5ba
tombrereton <tbrereton89@gmail.com>
2021-01-22 01:07:09 +0000

    Update logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import BigLink from '../components/BigLink/bigLink'
 
 export default () => (
   <div className={styles.home}>
-    <h1 className={styles.heading}>Hello, I'm <span className={styles.highlight}>Tom</span></h1>
+    <h1 className={styles.heading}>Hello, I'm <span className={`${styles.highlight} ${styles.underline}`}>Tom</span></h1>
     <h3 className={styles.subHeading}>Web Developer & Online Presence Consultant</h3>
     <h2 className={styles.sectionHeading}>How I Can <span className={styles.highlight}>Help</span></h2>
     <Service image={responsive} text={"Build you a fast, responsive and unique website. Optimised for both mobile and desktop."} />

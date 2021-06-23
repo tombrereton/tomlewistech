76526874bd4605d2b96790fc284e82dc47072448
Tom Brereton <tom.brereton@exify.net>
2021-06-23 22:52:11 +0100

    fix heading font

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -10,8 +10,8 @@ export default function Footer(props) {
     <div className={styles.footer}>
       <div className={styles.companySection}>
         <p className={styles.footerText}>Designed and built by<span className={styles.companyText}> Tom Lewis Tech</span></p>
-        <p className={styles.footerText}><FontAwesomeIcon icon="envelope"/> tom@tomlewis.tech</p>
-        <a className={styles.footerText} href="https://www.linkedin.com/in/tbrereton/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="1em"/> LinkedIn</a>
+        <p className={styles.footerText}><FontAwesomeIcon icon="envelope" /> tom@tomlewis.tech</p>
+        <a className={styles.footerText} href="https://www.linkedin.com/in/tbrereton/"><p className={styles.footerText}><FontAwesomeIcon icon={['fab', 'linkedin']} size="1em" /> LinkedIn</p></a>
       </div>
       <div className={styles.copyrightSection}>
         <img className={styles.copyrightImg} src={copyright} /><p className={styles.footerText}>Copyright 2020 Tom Lewis Tech Ltd</p>

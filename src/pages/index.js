0dcbd15b3483d6f53cfc4b56d8b78da9d2abd033
Tom Brereton <tom.brereton@exify.net>
2021-06-23 14:23:10 +0100

    Update services

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default () => (
       <h1 className={styles.heading}>Hello, I'm <span className={`${styles.highlight} ${styles.underline}`}>Tom.</span></h1>
       <h3 className={styles.subHeading}>Web Developer & Software Architect.</h3>
       <h2 className={styles.sectionHeading}>How I Can <span className={styles.highlight}>Help</span></h2>
-      <Service title={"Website & UI Design"} text={"These days most traffic is from a mobile, so we use a mobile-first approach and scale up."}>
+      <Service title={"Website & UI Design"} text={"Optimise for the modern landscape and use a mobile-first approach then scale up."}>
         <FontAwesomeIcon icon={faDesktop} size={"4x"} color={"#009B95"}/>
       </Service>
       <Service title={"User Testing & UX Design"} text={"Get feedback on how easy your website is to use before (or after) launch."}>
@@ -24,16 +24,16 @@ export default () => (
       <Service title={"Branding"} text={"Become recognisable."}>
         <FontAwesomeIcon icon={faEye} size={"4x"} color={"#009B95"}/>
       </Service>
-      <Service title={"Website Building"} text={"Ranging from a single page to a full e-commerce website."}>
+      <Service title={"Website Building"} text={"Websites can range from a single page to a full e-commerce website."}>
         <FontAwesomeIcon icon={faCog} size={"4x"} color={"#009B95"}/>
       </Service>
-      <Service title={"Search Engine Optimisation (SEO)"} text={"All websites are built with SEO optimisation so they appear in Google or other search providers."}>
+      <Service title={"Search Engine Optimisation (SEO)"} text={"Build websites with SEO optimisation so they appear in Google or other search providers."}>
         <FontAwesomeIcon icon={faChartArea} size={"4x"} color={"#009B95"}/>
       </Service>
       <Service title={"Company Email Setup"} text={"Give you a professional email domain through Google Suite or Office 365."}>
         <FontAwesomeIcon icon={faMailBulk} size={"4x"} color={"#009B95"}/>
       </Service>
-      <Service title={"Marketing"} text={"We can increase traffic to your website by setting up online ads through Google and Instagram."}>
+      <Service title={"Marketing"} text={"Increase traffic to your website by setting up online ads through Google and Instagram."}>
         <FontAwesomeIcon icon={faCameraRetro} size={"4x"} color={"#009B95"}/>
       </Service>
       <h2 className={styles.sectionHeading}>Some <span className={styles.highlight}>Projects</span></h2>
